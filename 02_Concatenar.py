37db712f36dab513dc3483cc2d907213e98c6cb0
Oscar Eduardo Caiza <62688482+OsEduIT@users.noreply.github.com>
2020-05-04 12:51:21 -0500

    Add files via upload

---
+++ 02_Concatenar.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  4 12:45:53 2020
+
+@author: oscar
+"""
+
+
+str1 = "Cisco"
+str2 = "Networking"
+str3 = "Academy"
+space = " "
+print(str1+space+str2+space+str3)
+print("\n"*2)
+print(str1+str2+str3)
+print("\n"*2)
+print(str1,str2,str3)
