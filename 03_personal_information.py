d43ed62ccc840a5e12e19c2bbe808d0341eb3bbb
Oscar Eduardo Caiza <62688482+OsEduIT@users.noreply.github.com>
2020-05-11 14:15:27 -0500

    Add files via upload

---
+++ 03_personal_information.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  4 15:08:15 2020
+
+@author: oscar
+"""
+
+firstName = input("What is your first name? ")
+lastName = input("What is your last name? ")
+location = input("What is your location? ")
+age = input("How old are you? ")
+space = " "
+print("Hello my friend " + firstName + space + lastName + "! tu vives en: " +
+      location + " y tienes " + age + " years old.")
---
+++ 04_if_else_vlan.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  4 15:36:36 2020
+
+@author: oscar
+"""
+
+
+nativeVLAN=1
+dataVLAN=100
+if nativeVLAN ==dataVLAN:
+    print("La VLAN nativa y la de datos son iguales")
+    
+else: 
+    print("La VLAN nativa y la de datos son diferentes")
